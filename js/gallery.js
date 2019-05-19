afa4bec1f31d4d762b3a0808d985748362b53b9b
hashir423 <50701802+hashir423@users.noreply.github.com>
2019-05-19 13:11:08 +0100

    Add files via upload
    Initial commit

---
+++ js/gallery.js
@@ -0,0 +1,16 @@
+const current = document.querySelector('#current');
+const imgs = document.querySelector('.imgs');
+const img = document.querySelectorAll('.imgs img');
+const opacity = 0.6;
+// Set first img opacity
+img[0].style.opacity = opacity;
+imgs.addEventListener('click', imgClick);
+
+
+function imgClick(e) {
+  img.forEach(img => (img.style.opacity = 1));
+  current.src = e.target.src;
+  current.classList.add('fade-in');
+  setTimeout(() => current.classList.remove('fade-in'), 500);
+  e.target.style.opacity = opacity;
+}
